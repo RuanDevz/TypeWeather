597ff751301034b79a5896e94b42ec8c4d5b1df5
RuanDevz <Ruanbatista1509@outlook.com>
2023-08-20 20:17:23 -0300

    parte do js implementado

---
+++ dashboard.js
@@ -0,0 +1,32 @@
+const hoursElement = document.getElementById("hours"); 
+const dateElement = document.getElementById("date"); 
+
+const currentDateTime = new Date();
+const currentHours = currentDateTime.getHours();
+const currentMinutes = currentDateTime.getMinutes();
+
+const formattedHours = String(currentHours).padStart(2, '0');
+const formattedMinutes = String(currentMinutes).padStart(2, '0');
+
+const timeString = `${formattedHours}:${formattedMinutes}`;
+
+hoursElement.textContent = timeString;
+
+const meses = [
+    "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+    "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+];
+
+const diasSemana = [
+    "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
+    "Quinta-feira", "Sexta-feira", "Sábado"
+];
+
+const diaSemana = diasSemana[currentDateTime.getDay()];
+const diaMes = currentDateTime.getDate();
+const mes = meses[currentDateTime.getMonth()];
+const ano = currentDateTime.getFullYear();
+
+const dateString = `${diaSemana}, ${diaMes} de ${mes} de ${ano}`;
+
+dateElement.textContent = dateString;
