593a1a4a9ce7bfc571e14e1d615ec0a2c529bdc5
RuanDevz <Ruanbatista1509@outlook.com>
2023-08-21 11:58:07 -0300

    API Adicionada

--- Javascript/dashboard.js
+++ Javascript/dashboard.js
@@ -1,3 +1,8 @@
+
+
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=99db18ed4405b31d392f6d9e808fe78f
+
+
 const hoursElement = document.getElementById("hours"); 
 const dateElement = document.getElementById("date"); 
 
@@ -30,3 +35,6 @@ const ano = currentDateTime.getFullYear();
 const dateString = `${diaSemana}, ${diaMes} de ${mes} de ${ano}`;
 
 dateElement.textContent = dateString;
+
+// EXTRAÇÃO DE DADOS DA API
+
--- Javascript/script.js
+++ Javascript/script.js
@@ -10,7 +10,6 @@ function ShowLoading() {
 function HideLoading() {
   loading.style.display = "none";
 }
-
 input.addEventListener('input', () => {
   const valorInput = input.value.toLowerCase();
   const sugestoesFiltradas = estados.filter(estado =>
@@ -26,11 +25,6 @@ input.addEventListener('input', () => {
     sugestaoItem.addEventListener('click', () => {
       input.value = estado;
       sugestoes.innerHTML = '';
-      ShowLoading();
-
-      setTimeout(() => {
-        HideLoading();
-      }, 10000);
     });
     sugestoes.appendChild(sugestaoItem);
   });
@@ -41,5 +35,34 @@ input.addEventListener('input', () => {
 document.addEventListener('click', event => {
   if (!input.contains(event.target)) {
     sugestoes.innerHTML = '';
+  }
+});
+const APIKey = "99db18ed4405b31d392f6d9e808fe78f";
+input.addEventListener('change', async () => {
+  const city = input.value;
+  const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+  try {
+    ShowLoading();
+    
+    const response = await fetch(weatherEndpoint);
+    const data = await response.json();
+    
+    if (data) {
+      const temperature = data.main.temp;
+      const weatherDescription = data.weather[0].description;
+      
+      const temperatureElement = document.getElementById("temperature");
+      const descriptionElement = document.getElementById("description");
+      
+      temperatureElement.textContent = `${temperature} °C`;
+      descriptionElement.textContent = weatherDescription;
+      
+      window.location.href = "clima.html";
+    }
+  } catch (error) {
+    console.error("Erro ao obter os dados do tempo:", error);
+  } finally {
+    HideLoading();
   }
 });
